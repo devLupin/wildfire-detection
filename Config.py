04b72f9559f711d3d5ff808450a68da96ce88c25
devLupin <luckyquit49@gmail.com>
2023-08-16 16:01:47 +0900

    add config file

---
+++ Config.py
@@ -0,0 +1,6 @@
+class Config:
+    MODEL = 'yolov8/runs3/train/weights/best.pt'
+    LOCATION_API = 'YOUR_LOCATION_API_KEY'
+    CONFIDENCE = 0.5
+    DURATION = 100      # ms
+    FREQ = 5000         # Hz
